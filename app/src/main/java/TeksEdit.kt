a46f738a9ab56ce810420ec77e8d275130c68e55
jaddu17 <shafajaddu@gmail.com>
2025-10-31 09:55:51 +0700

    menambahkan divider

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -85,6 +85,13 @@ fun FormDataDiri(modifier: Modifier)
             }
         )
 
+        Divider(
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium), top = dimensionResource(
+                id = R.dimen.padding_medium
+            )),
+            thickness = dimensionResource(id = R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
 
     }
 }
