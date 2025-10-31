40b6d20e404178336339a8db70df7ad65b5ec2e9
jaddu17 <shafajaddu@gmail.com>
2025-10-31 09:09:15 +0700

    membuat var untuk mengingat nilai yg di masukkan

---
+++ app/src/main/java/TeksEdit.kt
@@ -0,0 +1,14 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+
+fun FormDataDiri(modifier: Modifier)
+{
+    //variabel-variabel untuk mengingat nilai masukkan dari keyboard
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+}
