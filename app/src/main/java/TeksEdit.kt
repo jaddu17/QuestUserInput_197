b946df13df61edbbe19563d7369843b717341b6a
jaddu17 <shafajaddu@gmail.com>
2025-10-31 09:25:35 +0700

    membuat column

--- app/src/main/java/TeksEdit.kt
+++ app/src/main/java/TeksEdit.kt
@@ -1,9 +1,18 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 
@@ -20,4 +29,11 @@ fun FormDataDiri(modifier: Modifier)
     var jenis by remember {mutableStateOf("")}
 
     val gender:List<String> = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = Modifier.padding(top = 50.dp),
+        verticalArrangement = Arrangement.Top,
+
+        horizontalAlignment = Alignment.CenterHorizontally)
+        )
+    }
 }
