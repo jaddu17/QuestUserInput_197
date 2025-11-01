2b4c37935e7bacad5c5190930b4a47065e76751e
jaddu17 <shafajaddu@gmail.com>
2025-11-01 16:10:54 +0700

    membuat text dan radio button status

--- app/src/main/java/Tugas5.kt
+++ app/src/main/java/Tugas5.kt
@@ -124,6 +124,30 @@ fun FormPendaftaran(modifier: Modifier){
                     }
                 }
             }
+            Text(
+                text = "STATUS",
+                fontSize = 18.sp,
+                color = Color.Black,
+                modifier = Modifier
+                    .padding(top = 10.dp, start = 10.dp)
+            )
+            Column {
+                kawin.forEach { item ->
+                    Row(modifier = Modifier.selectable(
+                        selected = textSP == item,
+
+                        onClick = {textSP = item}
+                    ), verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = textSP == item,
+
+                            onClick = {
+                                textSP = item
+                            })
+                        Text(item)
+                    }
+                }
+            }
         }
     }
 }
