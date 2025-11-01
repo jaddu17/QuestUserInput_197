8460ccce86c4f7d78171775a0c503dcd8c3640d0
jaddu17 <shafajaddu@gmail.com>
2025-11-01 16:11:53 +0700

    membuat button submit

--- app/src/main/java/Tugas5.kt
+++ app/src/main/java/Tugas5.kt
@@ -168,6 +168,21 @@ fun FormPendaftaran(modifier: Modifier){
                     textAlamat = it
                 }
             )
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp, start = 10.dp, end = 10.dp),
+                enabled = textAlamat.isNotEmpty(),
+
+                onClick = {
+                    nama = textNama
+                    jenis = textJK
+                    status = textSP
+                    alamat = textAlamat
+                }
+            ){
+                Text(stringResource(R.string.submit))
+            }
         }
     }
 }
